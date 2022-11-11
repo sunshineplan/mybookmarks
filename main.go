6ce501e2e23c8e0599750e9d9338188a32502ccb
sunshineplan <sunshineplan@users.noreply.github.com>
2022-11-11 16:39:14 +0800

    Update

--- main.go
+++ main.go
@@ -96,36 +96,21 @@ func main() {
 	case 0:
 		run()
 	case 1:
-		switch flag.Arg(0) {
-		case "run":
-			svc.Run(false)
-		case "debug":
-			svc.Run(true)
-		case "test":
-			err = svc.Test()
-		case "install":
-			err = svc.Install()
-		case "remove":
-			err = svc.Remove()
-		case "start":
-			err = svc.Start()
-		case "stop":
-			err = svc.Stop()
-		case "restart":
-			err = svc.Restart()
-		case "update":
-			err = svc.Update()
-		case "add", "delete":
-			log.Fatalf("%s need two arguments", flag.Arg(0))
-		default:
-			log.Fatalln("Unknown argument:", flag.Arg(0))
+		cmd := flag.Arg(0)
+		var ok bool
+		if ok, err = svc.Command(cmd); !ok {
+			if cmd == "add" || cmd == "delete" {
+				log.Fatalf("%s need two arguments", cmd)
+			} else {
+				log.Fatalln("Unknown argument:", cmd)
+			}
 		}
 	case 2:
 		switch flag.Arg(0) {
 		case "add":
 			addUser(flag.Arg(1))
 		case "delete":
-			if utils.Confirm("Do you want to initialize database?", 3) {
+			if utils.Confirm("Do you want to delete this user?", 3) {
 				deleteUser(flag.Arg(1))
 			}
 		default:
